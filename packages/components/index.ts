9f248b9bec240fcf89343b447a5e3e345d03bbb0
zhengxi <453027626@qq.com>
2022-08-07 16:44:22 +0800

    first commit

---
+++ packages/components/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: 郑曦
+ * @Date: 2022-08-07 14:54:03
+ * @LastEditors: 郑曦
+ * @LastEditTime: 2022-08-07 15:29:57
+ * @FilePath: /myLibs/packages/components/index.js
+ * @Description: 描述
+ */
+import hook from "@zx/hooks";
+
+console.log(hook);
---
+++ packages/hooks/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: 郑曦
+ * @Date: 2022-08-07 14:54:14
+ * @LastEditors: 郑曦
+ * @LastEditTime: 2022-08-07 14:57:52
+ * @FilePath: /myLibs/packages/hooks/index.js
+ * @Description: 描述
+ */
+const hook = 1;
+
+export default hook;
